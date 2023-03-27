15ebfde5529315acfc402dc34f50422be7d6eddb
tirahman105 <tirahman105@gmail.com>
2023-03-27 19:48:59 +0600

    add data to local storage

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
     const addToCart = (id) =>{
-        console.log('Item added', id)
+        // console.log('Item added', id)
+        addToDb(id);
     }
 
-    const addToCartWithParam = () => addToCart(id);
+    // const addToCartWithParam = () => addToCart(id);
     return (
         <div className='product'>
             <h2>Buy this : {name}</h2>
             <p>Only for: $ {price}</p>
             <p>It has Id: {id}</p>
-            <button onClick={addToCartWithParam}>Add to cart</button>
+            {/* <button onClick={addToCartWithParam}>Add to cart</button> */}
             <button onClick={() => addToCart(id) }>Add to cart: shortcut</button>
             
         </div>
