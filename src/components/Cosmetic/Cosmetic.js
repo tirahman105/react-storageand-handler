c14a9a35b00c80659a3401705141960a68f0255a
tirahman105 <tirahman105@gmail.com>
2023-03-27 18:07:20 +0600

    create event handler with parameters

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+    const addToCart = (id) =>{
+        console.log('Item added', id)
+    }
+
+    const addToCartWithParam = () => addToCart(id);
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this : {name}</h2>
             <p>Only for: $ {price}</p>
             <p>It has Id: {id}</p>
+            <button onClick={addToCartWithParam}>Add to cart</button>
+            <button onClick={() => addToCart(id) }>Add to cart: shortcut</button>
             
         </div>
     );
